110bc4f1adc91331047af3aab5fdd77f2d1b9caf
Dong Min <focuspdm4383@naver.com>
2025-03-28 01:20:44 +0900

    Remove vcpkg from index

---
+++ include/server.h
@@ -0,0 +1,23 @@
+#ifndef HTTP_SERVER_H // 헤더가 포함된 적 있는지 검사
+#define HTTP_SERVER_H // 한 번만 포함되도록 설정
+
+#include <boost/asio.hpp> // 비동기 입출력 처리 라이브러리
+#include <string>
+#include <memory> // 스마트 포인터 라이브러리(메모리 관리 자동화 도움)
+
+class Server{
+    private:
+        void do_accept();
+        void handle_request(boost::asio::ip::tcp::socket socket);
+
+        boost::asio::io_context io_context_;
+        boost::asio::ip::tcp::acceptor acceptor_;
+
+    public:
+        Server(const std::string& address, unsigned short port);
+        void run();
+};
+
+
+
+#endif // 헤더 가드 종료
